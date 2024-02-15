6209f508d14cdc6df809bbd9bd78d52e66553d99
Delord-Nzanzu <wayirenzanzu@gmail.com>
2024-02-15 13:45:24 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `kali-academi-upgrade`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
