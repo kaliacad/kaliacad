3263d9b478daa208502aed8dd50284307a8e0592
Delord-Nzanzu <wayirenzanzu@gmail.com>
2024-03-22 11:52:49 +0100

    ajustage de nav bar au norme responsive

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -5,27 +5,71 @@ import Toolbar from "@mui/material/Toolbar";
 import NavBar from "./NavBar";
 // import NavBarDrawer from "./NavBarDrawer";
 import imagetitle from "../images/profile.jpg";
+import { Grid } from "@mui/material";
 // import { Stack, Typography } from "@mui/material";
 
 function Header() {
   return (
-    <AppBar
-      position="static"
-      elevation={0}
-      sx={{
-        backgroundImage: `url(${imagetitle})`,
-        height: "61vh",
-        objectFit: "cover",
-        backgroundPosition: "center top", // Ajustez la position de l'image selon vos besoins
-        backgroundSize: "cover",
-        display: "flex",
-        // opacity: 0.5,
-      }}
-    >
-      <Toolbar disableGutters sx={{ width: "100%" }}>
-        <NavBar />
-      </Toolbar>
-    </AppBar>
+    <div>
+      <Grid
+        container
+        display={{
+          xs: "none",
+          md: "flex",
+          sm: "none",
+          lg: "flex",
+          xl: "flex",
+        }}
+      >
+        <AppBar
+          position="static"
+          elevation={0}
+          sx={{
+            backgroundImage: `url(${imagetitle})`,
+            height: "61vh",
+            objectFit: "cover",
+            backgroundPosition: "center top", // Ajustez la position de l'image selon vos besoins
+            backgroundSize: "cover",
+            display: "flex",
+            // opacity: 0.5,
+            width: "100%",
+          }}
+        >
+          <Toolbar disableGutters sx={{ width: "100%" }}>
+            <NavBar />
+          </Toolbar>
+        </AppBar>
+      </Grid>
+      <Grid
+        container
+        display={{
+          xs: "felx",
+          md: "none",
+          sm: "flex",
+          lg: "none",
+          xl: "none",
+        }}
+      >
+        <AppBar
+          position="static"
+          elevation={0}
+          sx={{
+            backgroundImage: `url(${imagetitle})`,
+            height: "45vh",
+            objectFit: "cover",
+            // backgroundPosition: "center top",`` // Ajustez la position de l'image selon vos besoins
+            backgroundSize: "cover",
+            display: "flex",
+            // opacity: 0.5,
+            width: "100%",
+          }}
+        >
+          <Toolbar disableGutters sx={{ width: "100%" }}>
+            <NavBar />
+          </Toolbar>
+        </AppBar>
+      </Grid>
+    </div>
   );
 }
 export default Header;
