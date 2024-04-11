0db5c217f13660c6ac75fafcf3153fe6759fc6b4
BirushaNdegeya <142086785+BirushaNdegeya@users.noreply.github.com>
2024-04-11 14:16:12 +0300

    Fix render icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,24 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `kali-academi-upgrade`,
-    siteUrl: `https://www.yourdomain.tld`,
-    
+    title: "kali-academi-upgrade",
+    siteUrl: "https://www.yourdomain.tld",
   },
   plugins: [
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "/public/static/Kali_academy_-_Logo-gris-removebg-icon.png",
+        icons: [
+          {
+            src: "/public/static/Kali_academy_-_Logo-gris-removebg-icon.png",
+            sizes: "3848x1554", // icon size
+            type: "image/png", // Type MIME de l'image
+          },
+        ],
+      },
+    },
   ],
-}
+}; w
