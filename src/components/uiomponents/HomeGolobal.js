8a4832db40bb4bc4da667ff44f3c842931a17334
Delord <63587598+Delord-Nzanzu@users.noreply.github.com>
2024-05-29 12:58:20 +0200

    Add loader

--- src/components/uiomponents/HomeGolobal.js
+++ src/components/uiomponents/HomeGolobal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Homeaf from "./HomeaWatWeDo";
 import FooterFin from "../footer/HomeFooter";
 import About from "./About";
@@ -6,16 +6,36 @@ import WeDo from "./WeDo";
 import Header from "../header/Header";
 import ThankYou from "./ThankYou";
 import Layout from "../../theme/layout";
+import Skeletons from "../Loader";
 
 function HomeGolobal() {
+  const [loading, setLoading] = useState(true);
+  React.useEffect(() => {
+    const fetchData = () => {
+      setTimeout(() => {
+        setLoading(false); // Mettre à jour l'état pour indiquer que les données sont chargées
+      }, 3000); // Temps de chargement simulé (3 secondes)
+    };
+
+    fetchData();
+
+    // Nettoyage de l'effet
+    return () => clearTimeout(fetchData);
+  }, []);
   return (
     <Layout>
-      <Header />
-      <About />
-      <WeDo />
-      <Homeaf />
-      <ThankYou />
-      <FooterFin />
+      {loading === true ? (
+        <Skeletons />
+      ) : (
+        <div>
+          <Header />
+          <About />
+          <WeDo />
+          <Homeaf />
+          <ThankYou />
+          <FooterFin />
+        </div>
+      )}
     </Layout>
   );
 }
--- src/pages/supporters.js
+++ src/pages/supporters.js
@@ -17,7 +17,7 @@ const GoldSupports = GoldSupport.map((supporter, index) => (
     key={index}
     sx={{
       maxWidth: 250,
-      ml: 5,
+      // ml: 5,
     }}
     elevation={0}
   >
@@ -94,13 +94,28 @@ function supporters() {
         </Typography>
         <Grid item xs={12} sm={12} md={8} lg={8} xl={8} mt={5}>
           <Typography variant="h2">Nos Supporteurs</Typography>
-
+          <Typography mt={2} mb={3}>
+            Code.org is sincerely grateful for our vibrant community of
+            corporate, institutional, and individual donors. We acknowledge the
+            following supporters who have made generous commitments of $1,000
+            and above to support our 2023 fiscal year (January 1, 2023 -
+            December 31, 2023). For further details or to explore partnership
+            opportunities with Code.org, please contact our Office of
+            Development at giving@code.org. Code.org® believes that every
+            student in every school should have the opportunity to learn
+            computer science. Thanks to the support listed below, we are able to
+            increase access to computer science education globally. Code.org is
+            a 501(c)(3) nonprofit organization and our Federal Tax ID is
+            46-0858543.
+          </Typography>
+          <Typography variant="h3">Plantium Supporteurs ($10,000+)</Typography>
           <div
             style={{
               display: "flex",
               width: "100%",
               flexWrap: "wrap",
               marginTop: 20,
+              // background: "red",
             }}
           >
             {GoldSupports}
@@ -120,7 +135,7 @@ function supporters() {
                 fontWeight: "bold",
               }}
             >
-              Institutional Supporters
+              Supporteurs Institutionnels
             </Typography>
             <Typography
               sx={{
@@ -129,7 +144,7 @@ function supporters() {
                 marginLeft: 20,
               }}
             >
-              Individual Supporters
+              Supporteurs Individuels
             </Typography>
           </Stack>
           <Stack
@@ -276,6 +291,196 @@ function supporters() {
               </Stack>
             </Stack>
           </div>
+          <Typography mt={5} variant="h3">
+            Gold Supporteurs ($3,000+)
+          </Typography>
+          <Grid item xs={12} sm={12} md={8} lg={8} xl={8} mt={5}>
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                flexWrap: "wrap",
+                marginTop: 20,
+              }}
+            >
+              {GoldSupports}
+            </div>
+            <Stack
+              sx={{
+                bgcolor: "#00B2C0",
+                p: 2,
+                flexDirection: "row",
+                // justifyContent: "space-between",
+                mt: 3,
+                width: "145%",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#fff",
+                  fontWeight: "bold",
+                }}
+              >
+                Institutional Supporters
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#fff",
+                  fontWeight: "bold",
+                  marginLeft: 20,
+                }}
+              >
+                Individual Supporters
+              </Typography>
+            </Stack>
+            <Stack
+              sx={{
+                bgcolor: "#5B6770",
+                p: 2,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                mt: 0.5,
+                width: "145%",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  color: "#fff",
+                }}
+              >
+                $500+
+              </Typography>
+            </Stack>
+            <div
+              style={{
+                marginTop: 20,
+                marginBottom: 20,
+              }}
+            >
+              <Stack
+                sx={{
+                  flexDirection: "row",
+                  // justifyContent: "space-between",
+                }}
+              >
+                <Stack>
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+                <Stack
+                  sx={{
+                    marginLeft: 27,
+                  }}
+                >
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+              </Stack>
+            </div>
+            <Stack
+              sx={{
+                bgcolor: "#5B6770",
+                p: 2,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                mt: 0.5,
+                width: "145%",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  color: "#fff",
+                }}
+              >
+                $100+
+              </Typography>
+            </Stack>
+            <div
+              style={{
+                marginTop: 20,
+                marginBottom: 20,
+              }}
+            >
+              <Stack
+                sx={{
+                  flexDirection: "row",
+                  // justifyContent: "space-between",
+                }}
+              >
+                <Stack>
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+                <Stack
+                  sx={{
+                    marginLeft: 27,
+                  }}
+                >
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+              </Stack>
+            </div>
+            <Stack
+              sx={{
+                bgcolor: "#5B6770",
+                p: 2,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                mt: 0.5,
+                width: "145%",
+              }}
+            >
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  color: "#fff",
+                }}
+              >
+                $30+
+              </Typography>
+            </Stack>
+            <div
+              style={{
+                marginTop: 20,
+                marginBottom: 20,
+              }}
+            >
+              <Stack
+                sx={{
+                  flexDirection: "row",
+                  // justifyContent: "space-between",
+                }}
+              >
+                <Stack>
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+                <Stack
+                  sx={{
+                    marginLeft: 27,
+                  }}
+                >
+                  <ul>
+                    <li>Contributaire 1</li>
+                    <li>Contributaire 2</li>
+                  </ul>
+                </Stack>
+              </Stack>
+            </div>
+          </Grid>
         </Grid>
       </Grid>
     </Layout>
