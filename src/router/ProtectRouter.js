5d682a0d6d986b0937cf0c62bcf1b58bd746976b
Abel L MBULA <abel@kinshasadigital.com>
2024-08-04 17:42:11 +0200

    rm unnecessary pages and files organization

--- src/router/ProtectRouter.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-function ProtectRoutes() {
-  const [isAuth] = React.useState(localStorage.getItem("isAuth"));
-  return isAuth === "true" ? <Outlet /> : <Navigate to={"/error"} />;
-}
-
-export default ProtectRoutes;
