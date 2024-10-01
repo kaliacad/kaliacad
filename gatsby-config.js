51aa863cd8ea728a9a1c84923b8b6644c27b3b49
firmin nagnduli <finganduli@gmail.com>
2024-10-01 09:20:32 +0200

    fix: fix conflit in index.html

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,25 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `kali Academy`,
+    title: `Kali Academy`,
     siteUrl: `https://kaliacademy.org`,
   },
   plugins: [
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
     "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "/public/static/Kali_academy_-_Logo-gris-removebg-icon.png",
+        icons: [
+          {
+            src: "/public/static/Kali_academy_-_Logo-gris-removebg-icon.png",
+            sizes: "3848x1554",
+            type: "image/png",
+          },
+        ],
+      },
+    },
   ],
 };
